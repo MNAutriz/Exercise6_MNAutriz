85498992711242469b8aa42319be685efaa0b8ed
MNAutriz <mgautriz@up.edu.ph>
2024-03-14 15:40:34 +0800

    Added the editing for students in database

--- mongoose-basics.js
+++ mongoose-basics.js
@@ -31,3 +31,23 @@ const newStudent = new Student({
 });
 
 await newStudent.save();
+
+//Updating the database
+
+let stud = await Student.findOne({ age: 28});
+stud.fname = "Peter";
+await stud.save();
+
+// updates a single document and returns an object containing the result details (NOT the document).
+await Student.updateOne(
+  { age: 17 },
+  {$set: {fname: "John"}}
+);
+
+// updates multiple documents and returns an object containing the result details (NOT the documents)
+await Student.updateMany(
+  { age: 65 },
+  {$set: {age: 17}
+});
+
+
